532e2b059baca9f971e04691218621b8e86fe242
rohit3607 <rohitplayer87089@gmail.com>
2024-10-10 09:41:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -57,7 +57,7 @@ async def start_command(client: Client, message: Message):
             _, token = text.split("_", 1)
             if verify_status['verify_token'] != token:
                 return await message.reply("Your token is invalid or expired. Try again by clicking /start")
-            await update_verify_status(id, is_verified=True, verified_time={get_exp_time(VERIFY_EXPIRE)}())
+            await update_verify_status(id, is_verified=True, verified_time=get_exp_time(VERIFY_EXPIRE)())
             await message.reply(
                 "Your token successfully verified and valid for: {get_exp_time(VERIFY_EXPIRE)} ", 
                 reply_markup=PREMIUM_BUTTON,
